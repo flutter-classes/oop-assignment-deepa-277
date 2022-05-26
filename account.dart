0676344029af1b13a6c0a988ca74a86a844ec640
deepa-277 <deepagautam.277@gmail.com>
2022-05-26 18:33:16 +0545

    assignment

--- account.dart
+++ account.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 class Account {
   int acNo;
   String acType;
@@ -12,5 +14,35 @@ class Account {
   accDetail() {
     print("Account Number: $acNo");
     print("Account Type: $acType");
+    print("Account Balance: $acBalance");
+    return;
   }
+
+  withDraw() {
+    print("Enter your amount to withdraw...");
+    double withdraw = double.parse(stdin.readLineSync()!);
+    var balance = this.acBalance - withdraw;
+    if (balance <= 1000) {
+      print("Not Sufficient Balance...");
+    } else {
+      print("The amount withdrawn is $withdraw");
+    }
+    return;
+  }
+
+  depoBalance() {
+    print("Enter the amount you wish to deposite...");
+    double amount = double.parse(stdin.readLineSync()!);
+    var balance = this.acBalance + amount;
+    print("Succefully Deposited...");
+    print("Your Balance is now $balance");
+    return;
+  }
+}
+
+main() {
+  var acc = Account(acNo: 1234567890, acType: "Saving", acBalance: 1000);
+  acc.accDetail();
+  acc.depoBalance();
+  acc.withDraw();
 }
